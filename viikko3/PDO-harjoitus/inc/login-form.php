b5c78d6fb80f2475bbfa4a1ee567e2ec765edb2e
jannepeh <jannepeh@metropolia.fi>
2025-04-04 13:50:26 +0300

    added login and register forms

---
+++ viikko3/PDO-harjoitus/inc/login-form.php
@@ -0,0 +1,13 @@
+<section>
+    <form>
+        <div class="form-control">
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username"/>
+        </div>
+        <div class="form-control">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password"/>
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</section>
---
+++ viikko3/PDO-harjoitus/inc/register-form.php
@@ -0,0 +1,17 @@
+<section>
+    <form>
+        <div class="form-control">
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username"/>
+        </div>
+        <div class="form-control">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password"/>
+        </div>
+        <div class="form-control">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email"/>
+        </div>
+        <button type="submit">Register</button>
+    </form>
+</section>
