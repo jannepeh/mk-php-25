ec0b3fee5493a39621500b3c2d3e612dd500c61f
jannepeh <jannepeh@metropolia.fi>
2025-04-04 13:36:40 +0300

    Added the latest stuff

---
+++ viikko3/PDO-harjoitus/config/config.php
@@ -0,0 +1,2 @@
+<?php
+$SITE_URL = 'http://localhost/mk-php-25/viikko3/PDO-harjoitus';
---
+++ viikko3/PDO-harjoitus/inc/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--suppress HtmlUnknownTarget -->
+    <link href="./css/style.css" rel="stylesheet">
+    <!--suppress HtmlUnknownTarget -->
+    <script src="./js/main.js" defer></script>
+    <title>Media Items</title>
+</head>
+<body>
+<div class="container">
